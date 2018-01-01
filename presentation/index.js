9bf2a29bd000d68796af39a3ccf174ed5018e946
François Rosato <francois.rosato@ekino.com>
2018-01-01 16:44:12 +0100

    [+] Introduction

--- presentation/index.js
+++ presentation/index.js
@@ -1,70 +1,315 @@
 // Import React
-import React from "react";
+import React from 'react';
 
 // Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
+  CodePane,
+  ComponentPlayground,
   Deck,
+  Fill,
   Heading,
+  Image,
+  Layout,
+  Link,
   ListItem,
   List,
+  Markdown,
+  MarkdownSlides,
   Quote,
   Slide,
-  Text
-} from "spectacle";
+  SlideSet,
+  TableBody,
+  TableHeader,
+  TableHeaderItem,
+  TableItem,
+  TableRow,
+  Table,
+  Text,
+  GoToAction
+} from 'spectacle';
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default';
 
 // Require CSS
-require("normalize.css");
+require('normalize.css');
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const images = {
+  city:
+    'http://www.hdfmagazine.com/wp-content/uploads/2014/12/Caroline-Gutman-medium.jpeg',
+  kat:
+    'http://www.hdfmagazine.com/wp-content/uploads/2014/12/Caroline-Gutman-medium.jpeg',
+  logo:
+    'http://www.hdfmagazine.com/wp-content/uploads/2014/12/Caroline-Gutman-medium.jpeg',
+  markdown:
+    'http://www.hdfmagazine.com/wp-content/uploads/2014/12/Caroline-Gutman-medium.jpeg',
+  intro: {
+    bradFrost: require('./../assets/intro_brad_frost.jpg'),
+    devices: require('./../assets/intro_devices.png'),
+    screen_fragmentation: require('./../assets/intro_screen_fragmentation.jpg')
+  },
+  atomic: {
+    atoms: require('./../assets/atomic_icon-atom.png'),
+    molecules: require('./../assets/atomic_icon-molecule.png'),
+    organisms: require('./../assets/atomic_icon-organism.png'),
+    templates: require('./../assets/atomic_icon-template.png'),
+    pages: require('./../assets/atomic_icon-page.png'),
+    flow: require('./../assets/atomic_icon-flow.png'),
+    sample: {
+      atoms: require('./../assets/atomic_sample_atom.png'),
+      molecules: require('./../assets/atomic_sample_molecule.png'),
+      organisms: require('./../assets/atomic_sample_organism.png'),
+      templates: require('./../assets/atomic_sample_template.png'),
+      pages: require('./../assets/atomic_sample_page.png')
+    }
+  }
+};
+
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quarternary: '#CECECE'
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica'
+  }
+);
 
 export default class Presentation extends React.Component {
+  state = {
+    steps: 0
+  };
+
+  updateSteps = steps => {
+    if (this.state.steps !== steps) {
+      // eslint-disable-line no-invalid-this
+      this.setState({ steps }); // eslint-disable-line no-invalid-this
+    }
+  };
+
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
+      <Deck
+        transition={['zoom', 'slide']}
+        transitionDuration={500}
+        theme={theme}
+      >
+        {/* INTRO */}
+        <Slide transition={['zoom']} bgColor="primary" notes="@TODO">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            Atomic Design
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Craft effective interface design systems
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+
+        {/* INTRO - Screens */}
+        <Slide
+          transition={['slide']}
+          notes={`
+                * We used to work targeting specific screen size<br/>
+                * Many designs to validate<br/>
+                * Screen fragmentation keeps growing<br/>
+            `}
+        >
+          <Image src={images.intro.devices.replace('/', '')} />
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+
+        <Slide
+          notes={`
+                * As the craft of Web design continues to evolves, we're
+                recognizing the need to develop thoughtful design systems rather
+                than creating simple collection of web pages...<br />
+                * Atomic design is methodology for creating design systems.
+                * Let's focus on what's compose the page !<br/>
+            `}
+        >
+          <Image src={images.intro.screen_fragmentation.replace('/', '')} />
+          <Text>Screen fragmentation - @TODO add source</Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+
+        {/* INTRO - Brad Frost */}
+        <Slide
+          transition={['slide']}
+          bgImage={images.intro.bradFrost.replace('/', '')}
+          bgDarken={0.75}
+          notes={`
+                * Who is Brad Frost<br/>
+                * Designer<br/>
+                * Introduce Atomic design in 2013 - Tech Crunch<br/>
+                * Bio/Chemistry analogy<br/><br/>
+                * whether solid, liquid, gas, simple, complex, etc<br/>
+            `}
+        >
+          <Layout>
+            <BlockQuote>
+              <Quote size={2}>
+                The thought is that all matter is comprised of atoms.
+              </Quote>
+              <Cite>
+                Brad Frost
+                <Link href="http://atomicdesign.bradfrost.com/">
+                  <Text textColor="tertiary">Atomic Design</Text>
+                </Link>
+              </Cite>
+            </BlockQuote>
+          </Layout>
+        </Slide>
+
+        {/* ATOMIC - Components */}
+        <Slide transition={['slide']} notes={``}>
+          <Image src={images.atomic.flow.replace('/', '')} />
+          <Text
+            style={{
+              fontSize: '1.96rem',
+              marginTop: '40px',
+              marginBottom: '40px'
+            }}
+          >
+            "Atomic design is not a linear process, but rather a mental model to
+            help us think of our user interfaces as both a cohesive whole and a
+            collection of parts at the same time".
+          </Text>
+        </Slide>
+
+        {/* ATOMIC - Atoms */}
+        <Slide
+          transition={['slide']}
+          notes={`
+                * Indivisible<br/>
+                * No functional role on it's own<br/>
+            `}
+        >
+          {' '}
+          <Image src={images.atomic.atoms.replace('/', '')} width="100px" />
+          <Text
+            style={{
+              fontSize: '1.96rem',
+              marginTop: '40px',
+              marginBottom: '40px'
+            }}
+          >
+            <b>Atoms</b> are basic building blocks, Applied to web interface
+            atoms are our HTML tags, such as an input text field or a button.
+          </Text>
+          <Image
+            src={images.atomic.sample.atoms.replace('/', '')}
+            width="500px"
+          />
+        </Slide>
+
+        {/* ATOMIC - Molecules */}
+        <Slide transition={['slide']} notes={``}>
+          <Image src={images.atomic.molecules.replace('/', '')} width="100px" />
+          <Text
+            style={{
+              fontSize: '1.96rem',
+              marginTop: '40px',
+              marginBottom: '40px'
+            }}
+          >
+            In interfaces, <b>molecules</b> are relatively simple groups of UI
+            elements functioning together as a unit. For example, a form label,
+            search input, and button can join together to create a search form{' '}
+            <b>molecule</b>.
+          </Text>
+          <Image
+            src={images.atomic.sample.molecules.replace('/', '')}
+            width="500px"
+          />
+        </Slide>
+
+        {/* ATOMIC - Organisms */}
+        <Slide transition={['slide']} notes={``}>
+          <Image src={images.atomic.organisms.replace('/', '')} width="100px" />
+          <Text
+            style={{
+              fontSize: '1.96rem',
+              marginTop: '40px',
+              marginBottom: '40px'
+            }}
+          >
+            <b>Organisms</b> are relatively complex UI components composed of
+            groups of molecules and/or atoms and/or other organisms. These
+            organisms form distinct sections of an interface.
+          </Text>
+          <Image
+            src={images.atomic.sample.organisms.replace('/', '')}
+            width="500px"
+          />
+        </Slide>
+
+        {/* ATOMIC - Template */}
+        <Slide
+          transition={['slide']}
+          notes={`
+            * Say goodbye to our chemistry analogy <br/>
+        `}
+        >
+          <Image src={images.atomic.templates.replace('/', '')} width="100px" />
+          <Text
+            style={{
+              fontSize: '1.96rem',
+              marginTop: '40px',
+              marginBottom: '40px'
+            }}
+          >
+            <b>Templates</b> are page-level objects that place components into a
+            layout and articulate the design’s underlying content structure.
+          </Text>
+          <Image
+            src={images.atomic.sample.templates.replace('/', '')}
+            width="500px"
+          />
+        </Slide>
+
+        {/* ATOMIC - Page */}
+        <Slide transition={['slide']} notes={``}>
+          <Image src={images.atomic.pages.replace('/', '')} width="100px" />
+          <Text
+            style={{
+              fontSize: '1.96rem',
+              marginTop: '40px',
+              marginBottom: '40px'
+            }}
+          >
+            <b>Pages</b> are specific instances of templates that show what a UI
+            looks like with real representative content in place.
+          </Text>
+          <Image
+            src={images.atomic.sample.pages.replace('/', '')}
+            width="500px"
+          />
+        </Slide>
+
+        {/* ADVANTAGES */}
+        <Slide bgColor="secondary" transition={['slide']} notes={``}>
+          <Image src={images.atomic.pages.replace('/', '')} width="100px" />
+          <Heading textColor="tertiary" fit size={5}>
+            Advantage
+          </Heading>
+          <Text
+            textColor="primary"
+            style={{
+              fontSize: '1.96rem',
+              marginTop: '40px',
+              marginBottom: '40px'
+            }}
+          >
+            <b>Pages</b> are specific instances of templates that show what a UI
+            looks like with real representative content in place.
+          </Text>
+          <Image
+            src={images.atomic.sample.pages.replace('/', '')}
+            width="500px"
+          />
         </Slide>
       </Deck>
     );
---
+++ presentation/sources.js
@@ -0,0 +1,331 @@
+import {MarkdownSlides} from "spectacle"
+
+{/* INTRO - Old way */}
+<Slide transition={['slide']} bgColor="tertiary" notes="@TODO">
+    <Heading caps size={6} textColor="primary">
+        How we use to do ?
+    </Heading>
+    <Heading size={4} textColor="secondary" margin="20px 0px 0px">
+        Build "pages" or "screens"
+    </Heading>
+    <Text size={6} textColor="secondary">
+        Standard text
+    </Text>
+</Slide>
+
+<Slide transition={['fade']} bgColor="tertiary">
+    <Heading size={6} textColor="primary" caps>
+Typography
+</Heading>
+<Heading size={1} textColor="secondary">
+    Heading 1
+</Heading>
+<Heading size={2} textColor="secondary">
+    Heading 2
+</Heading>
+<Heading size={3} textColor="secondary">
+    Heading 3
+</Heading>
+<Heading size={4} textColor="secondary">
+    Heading 4
+</Heading>
+<Heading size={5} textColor="secondary">
+    Heading 5
+</Heading>
+<Text size={6} textColor="secondary">
+    Standard text
+</Text>
+</Slide>
+<Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+    <Heading size={6} textColor="secondary" caps>
+        Standard List
+    </Heading>
+    <List>
+    <ListItem>Item 1</ListItem>
+    <ListItem>Item 2</ListItem>
+    <ListItem>Item 3</ListItem>
+    <ListItem>Item 4</ListItem>
+    </List>
+</Slide>
+<Slide transition={['fade']} bgColor="secondary" textColor="primary">
+    <BlockQuote>
+        <Quote>Example Quote</Quote>
+        <Cite>Author</Cite>
+    </BlockQuote>
+    </Slide>
+    <Slide transition={['zoom']} bgColor="primary">
+        <Heading size={1} fit caps lineHeight={1} textColor="black">
+            Spectacle
+        </Heading>
+        <Heading size={1} fit caps>
+            A ReactJS Presentation Library
+        </Heading>
+        <Heading size={1} fit caps textColor="black">
+            Where You Can Write Your Decks In JSX
+        </Heading>
+        <Link href="https://github.com/FormidableLabs/spectacle">
+            <Text bold caps textColor="tertiary">
+                View on Github
+            </Text>
+        </Link>
+        <Text textSize="1.5em" margin="20px 0px 0px" bold>
+            Hit Your Right Arrow To Begin!
+        </Text>
+    </Slide>
+    <Slide
+    onActive={slideIndex => {
+    console.info(`Viewing slide index: ${slideIndex}.`); // eslint-disable-line no-console
+    }}
+    id="wait-what"
+    goTo={4}
+    transition={[
+    'fade',
+    (transitioning, forward) => {
+    const angle = forward ? -180 : 180;
+    return {
+    transform: `
+                  translate3d(0%, ${transitioning ? 100 : 0}%, 0)
+                  rotate(${transitioning ? angle : 0}deg)
+                `,
+    backgroundColor: transitioning ? '#26afff' : '#000'
+    };
+    }
+    ]}
+    bgColor="black"
+    notes="You can even put notes on your slide. How awesome is that?"
+    >
+    <Image src={images.kat.replace('/', '')} margin="0px auto 40px" />
+    <Heading size={2} caps fit textColor="primary" textFont="primary">
+    Wait what?
+</Heading>
+</Slide>
+<Slide
+    transitionIn={['zoom', 'fade']}
+    transitionOut={['slide', 'fade']}
+    bgColor="primary"
+    notes="<ul><li>talk about that</li><li>and that</li></ul>"
+>
+    <CodePane
+        lang="jsx"
+        source={require('raw-loader!../assets/deck.example')}
+        margin="20px auto"
+        overflow="overflow"
+    />
+</Slide>
+<Slide goTo={3}>
+    <ComponentPlayground theme="dark" />
+</Slide>
+
+<Slide>
+    <Heading size={2} textColor="secondary" margin="0.25em">
+        Mix it up!
+    </Heading>
+    <Heading size={6} textColor="tertiary">
+        You can even jump to different slides with a standard button or
+        custom component!
+    </Heading>
+    <GoToAction margin="1em" slide={8}>
+        Jump to Slide 8
+    </GoToAction>
+    <GoToAction
+        render={goToSlide => (
+            <select
+                defaultValue=""
+                style={{
+                    background: '#000',
+                    color: '#fff',
+                    fontFamily: theme.print.fonts.primary,
+                    fontSize: '1.1em'
+                }}
+                onChange={({ target }) => goToSlide(target.value)}
+            >
+                <option value="" disabled>
+                    Custom Slide Picker
+                </option>
+                <option value="wait-what">Wait What!? Slide</option>
+                <option value={3}>Slide 3</option>
+            </select>
+        )}
+    />
+</Slide>
+<Slide
+    transition={['slide']}
+    bgDarken={0.75}
+    getAppearStep={this.updateSteps}
+>
+    <Appear>
+        <Heading size={1} caps textColor="tertiary">
+            Can
+        </Heading>
+        </Appear>
+        <Appear>
+            <Heading size={1} caps textColor="secondary">
+                Count
+            </Heading>
+        </Appear>
+        <Appear>
+        <Heading size={1} caps textColor="tertiary">
+        Steps
+        </Heading>
+    </Appear>
+    <Heading size={1} caps fit textColor="secondary">
+        Steps: {this.state.steps}
+    </Heading>
+</Slide>
+<Slide transition={['zoom', 'fade']} bgColor="primary">
+    <Heading caps fit>
+        Flexible Layouts
+    </Heading>
+    <Layout>
+        <Fill>
+            <Heading
+                size={4}
+                caps
+                textColor="secondary"
+                bgColor="white"
+                margin={10}
+            >
+                Left
+            </Heading>
+        </Fill>
+        <Fill>
+        <Heading
+        size={4}
+        caps
+        textColor="secondary"
+        bgColor="white"
+        margin={10}
+        >
+        Right
+        </Heading>
+    </Fill>
+</Layout>
+</Slide>
+<Slide transition={['slide']} bgColor="black">
+    <BlockQuote>
+        <Quote>Wonderfully formatted quotes</Quote>
+        <Cite>Ken Wheeler</Cite>
+    </BlockQuote>
+</Slide>
+<Slide
+    transition={['spin', 'zoom']}
+    bgColor="tertiary"
+    controlColor="primary"
+    progressColor="primary"
+>
+    <Heading caps fit size={1} textColor="primary">
+        Inline Markdown
+    </Heading>
+    <Markdown>
+        {`
+  ![Markdown Logo](${images.markdown.replace('/', '')})
+  You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
+  * Lists too!
+  * With ~~strikethrough~~ and _italic_
+  * And let's not forget **bold**
+            `}
+    </Markdown>
+</Slide>
+{MarkdownSlides`
+#### Create Multiple Slides in Markdown
+All the same tags and elements supported in <Markdown /> are supported in MarkdownSlides.
+---
+Slides are separated with **three dashes** and can be used _anywhere_ in the deck. The markdown can either be:
+* A Tagged Template Literal
+* Imported Markdown from another file
+          `}
+<Slide transition={['slide', 'spin']} bgColor="primary">
+    <Heading caps fit size={1} textColor="tertiary">
+        Smooth
+    </Heading>
+    <Heading caps fit size={1} textColor="secondary">
+        Combinable Transitions
+    </Heading>
+</Slide>
+<SlideSet>
+    <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <List>
+            <Appear>
+                <ListItem>Inline style based theme system</ListItem>
+            </Appear>
+            <Appear>
+            <ListItem>Autofit text</ListItem>
+        </Appear>
+        <Appear>
+            <ListItem>Flexbox layout system</ListItem>
+            </Appear>
+            <Appear>
+                <ListItem>PDF export</ListItem>
+            </Appear>
+            <Appear>
+            <ListItem>And...</ListItem>
+    </Appear>
+</List>
+</Slide>
+<Slide transition={['slide']} bgColor="primary">
+    <Heading size={1} caps fit textColor="tertiary">
+        Your presentations are interactive
+    </Heading>
+</Slide>
+</SlideSet>
+<Slide
+    transition={['slide']}
+    bgColor="primary"
+    notes="Hard to find cities without any pizza"
+>
+    <Heading
+        size={4}
+        caps
+        textColor="secondary"
+        bgColor="white"
+        margin={10}
+    >
+        Pizza Toppings
+    </Heading>
+    <Layout>
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHeaderItem />
+                    <TableHeaderItem>2011</TableHeaderItem>
+                    <TableHeaderItem>2013</TableHeaderItem>
+                    <TableHeaderItem>2015</TableHeaderItem>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableItem>None</TableItem>
+                    <TableItem>61.8%</TableItem>
+                <TableItem>39.6%</TableItem>
+                <TableItem>35.0%</TableItem>
+            </TableRow>
+            <TableRow>
+                <TableItem>Pineapple</TableItem>
+                <TableItem>28.3%</TableItem>
+                <TableItem>54.5%</TableItem>
+                <TableItem>61.5%</TableItem>
+            </TableRow>
+            <TableRow>
+                <TableItem>Pepperoni</TableItem>
+                <TableItem />
+                <TableItem>50.2%</TableItem>
+                <TableItem>77.2%</TableItem>
+            </TableRow>
+            <TableRow>
+                <TableItem>Olives</TableItem>
+                <TableItem />
+                <TableItem>24.9%</TableItem>
+                <TableItem>55.9%</TableItem>
+            </TableRow>
+        </TableBody>
+    </Table>
+</Layout>
+</Slide>
+<Slide transition={['spin', 'slide']} bgColor="tertiary">
+    <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
+        Made with love in Seattle by
+    </Heading>
+    <Link href="http://www.formidable.com">
+        <Image width="100%" src={images.logo} />
+    </Link>
+</Slide>
